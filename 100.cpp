a59ca89c283081b66880344684e486d29075109f
TiChuot97 <daohoanglan2341997@gmail.com>
2015-11-18 16:22:09 -0600

    100 submission

---
+++ 100.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+const int INF = 23041997;
+
+#define ll long long
+
+int a, b;
+
+int main() {
+    cin >> a >> b;
+    cout << a + b;
+}
